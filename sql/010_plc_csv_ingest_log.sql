87aae5a2c57edddf12b84ee8e1d4d325a2e73ae1
ryanm-plastic-recycling <111075202+ryanm-plastic-recycling@users.noreply.github.com>
2026-02-27 09:18:44 -0500

    Update 010_plc_csv_ingest_log.sql

--- sql/010_plc_csv_ingest_log.sql
+++ sql/010_plc_csv_ingest_log.sql
@@ -1,30 +1,33 @@
-IF OBJECT_ID('dbo.KYZ_PlcCsvIngestLog', 'U') IS NULL
+/* sql/010_plc_csv_ingest_log.sql */
+
+IF OBJECT_ID(N'dbo.KYZ_PlcCsvIngestLog', N'U') IS NULL
 BEGIN
     CREATE TABLE dbo.KYZ_PlcCsvIngestLog (
-        FilePath NVARCHAR(1024) NOT NULL,
-        FileSizeBytes BIGINT NOT NULL,
-        LastWriteTimeUtc DATETIME2(3) NOT NULL,
-        Sha256 CHAR(64) NULL,
-        ProcessedAtUtc DATETIME2(3) NOT NULL,
-        Status NVARCHAR(16) NOT NULL,
-        RowCount INT NOT NULL,
-        IntervalMin DATETIME2(0) NULL,
-        IntervalMax DATETIME2(0) NULL,
-        ErrorMessage NVARCHAR(4000) NULL,
+        FilePath           NVARCHAR(450)  NOT NULL,  -- indexed PK-safe
+        FileSizeBytes      BIGINT         NOT NULL,
+        LastWriteTimeUtc   DATETIME2(3)   NOT NULL,
+        Sha256             CHAR(64)       NULL,
+        ProcessedAtUtc     DATETIME2(3)   NOT NULL,
+        IngestStatus       NVARCHAR(16)   NOT NULL,  -- renamed (avoid keyword risk)
+        IngestRowCount     INT            NOT NULL,  -- renamed (avoid ROWCOUNT keyword risk)
+        IntervalMin        DATETIME2(0)   NULL,
+        IntervalMax        DATETIME2(0)   NULL,
+        ErrorMessage       NVARCHAR(4000) NULL,
         CONSTRAINT PK_KYZ_PlcCsvIngestLog PRIMARY KEY CLUSTERED (FilePath)
     );
-END
+END;
 GO
 
 IF NOT EXISTS (
-    SELECT 1 FROM sys.indexes
-    WHERE object_id = OBJECT_ID('dbo.KYZ_PlcCsvIngestLog')
-      AND name = 'IX_KYZ_PlcCsvIngestLog_ProcessedAtUtc'
+    SELECT 1
+    FROM sys.indexes
+    WHERE object_id = OBJECT_ID(N'dbo.KYZ_PlcCsvIngestLog')
+      AND name = N'IX_KYZ_PlcCsvIngestLog_ProcessedAtUtc'
 )
 BEGIN
     CREATE INDEX IX_KYZ_PlcCsvIngestLog_ProcessedAtUtc
         ON dbo.KYZ_PlcCsvIngestLog (ProcessedAtUtc DESC);
-END
+END;
 GO
 
 CREATE OR ALTER VIEW dbo.vw_KYZ_PlcCsvIngestLatest
@@ -35,8 +38,8 @@ SELECT TOP (1)
     LastWriteTimeUtc,
     Sha256,
     ProcessedAtUtc,
-    Status,
-    RowCount,
+    IngestStatus,
+    IngestRowCount,
     IntervalMin,
     IntervalMax,
     ErrorMessage
